edf434569dd11c904ec3d25175cd92ac78826729
Georges Basile Stavracas Neto <georges.stavracas@gmail.com>
2025-04-14 12:02:51 -0300

    lib: Add internal BsDeviceProvider interface
    This is not exposed publicly, and allows internal plugins to provide
    devices.

---
+++ lib/bs-device-provider-private.h
@@ -0,0 +1,38 @@
+/*
+ * bs-device-provider.h
+ *
+ * Copyright 2025 Georges Basile Stavracas Neto <georges.stavracas@gmail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ */
+
+#pragma once
+
+#include "bs-utils-private.h"
+
+#include <gio/gio.h>
+
+G_BEGIN_DECLS
+
+#define BS_TYPE_DEVICE_PROVIDER (bs_device_provider_get_type ())
+G_DECLARE_INTERFACE (BsDeviceProvider, bs_device_provider, BS, DEVICE_PROVIDER, GListModel)
+
+struct _BsDeviceProviderInterface
+{
+  GTypeInterface parent;
+};
+
+G_END_DECLS
