52dd0f1f4b7a32a6f117f8a1f6c045e74496269d
Georges Basile Stavracas Neto <georges.stavracas@gmail.com>
2025-06-20 11:20:59 -0300

    lib/renderer: Use libjpeg to convert to JPEG
    BMP still uses the gdk-pixbuf code path, but the JPEG case we convert
    the texture directly.

--- lib/bs-renderer.c
+++ lib/bs-renderer.c
@@ -21,6 +21,10 @@
 #include "bs-icon.h"
 #include "bs-renderer-private.h"
 
+#include <jpeglib.h>
+#include <jerror.h>
+#include <setjmp.h>
+
 struct _BsRenderer
 {
   GObject parent_instance;
@@ -30,6 +34,131 @@ struct _BsRenderer
 };
 
 G_DEFINE_FINAL_TYPE (BsRenderer, bs_renderer, G_TYPE_OBJECT)
+
+/*
+ * JPEG conversion
+ *
+ * Copied from gdkjpeg.c
+ */
+
+#define JPEG_MEM_DEST_USES_SIZE_T                                 \
+  (!(LIBJPEG_TURBO_VERSION_NUMBER) &&                             \
+   (((JPEG_LIB_VERSION) > 90) ||                                  \
+    ((JPEG_LIB_VERSION) == 90 && (JPEG_LIB_VERSION_MAJOR) > 9) || \
+    ((JPEG_LIB_VERSION) == 90 && (JPEG_LIB_VERSION_MAJOR) == 9 && (JPEG_LIB_VERSION_MINOR) > 3)))
+
+struct error_handler_data
+{
+  struct jpeg_error_mgr pub;
+  sigjmp_buf setjmp_buffer;
+  GError **error;
+};
+
+G_GNUC_NORETURN static void
+fatal_error_handler (j_common_ptr cinfo)
+{
+  struct error_handler_data *errmgr;
+  char buffer[JMSG_LENGTH_MAX];
+
+  errmgr = (struct error_handler_data *) cinfo->err;
+
+  cinfo->err->format_message (cinfo, buffer);
+
+  if (errmgr->error && *errmgr->error == NULL)
+    g_set_error (errmgr->error,
+                 GDK_TEXTURE_ERROR,
+                 GDK_TEXTURE_ERROR_CORRUPT_IMAGE,
+                 "Error interpreting JPEG image file (%s)", buffer);
+
+  siglongjmp (errmgr->setjmp_buffer, 1);
+
+  g_assert_not_reached ();
+}
+
+static void
+output_message_handler (j_common_ptr cinfo)
+{
+  /* do nothing */
+}
+
+static GBytes *
+convert_to_jpeg (GdkTexture *texture)
+{
+  g_autoptr (GdkTextureDownloader) downloader = NULL;
+  struct jpeg_compress_struct info;
+  struct error_handler_data jerr;
+  struct jpeg_error_mgr err;
+  guchar *data = NULL;
+#if JPEG_MEM_DEST_USES_SIZE_T
+  gsize size = 0;
+#else
+  gulong size = 0;
+#endif
+  guchar *input = NULL;
+  GBytes *texbytes = NULL;
+  const guchar *texdata;
+  gsize texstride;
+  guchar *row;
+  int width, height;
+
+  width = gdk_texture_get_width (texture);
+  height = gdk_texture_get_height (texture);
+
+  info.err = jpeg_std_error (&jerr.pub);
+  jerr.pub.error_exit = fatal_error_handler;
+  jerr.pub.output_message = output_message_handler;
+  jerr.error = NULL;
+
+  if (sigsetjmp (jerr.setjmp_buffer, 1))
+    {
+      free (data);
+      g_free (input);
+      jpeg_destroy_compress (&info);
+      g_clear_pointer (&texbytes, g_bytes_unref);
+      return NULL;
+    }
+
+  info.err = jpeg_std_error (&err);
+  jpeg_create_compress (&info);
+
+  info.image_width = width;
+  info.image_height = height;
+  info.input_components = 3;
+  info.in_color_space = JCS_RGB;
+
+  jpeg_set_defaults (&info);
+  jpeg_set_quality (&info, 96, TRUE);
+
+  info.mem->max_memory_to_use = 300 * 1024 * 1024;
+
+  jpeg_mem_dest (&info, &data, &size);
+
+  downloader = gdk_texture_downloader_new (texture);
+  gdk_texture_downloader_set_format (downloader, GDK_MEMORY_R8G8B8);
+  gdk_texture_downloader_set_color_state (downloader, gdk_color_state_get_srgb ());
+  texbytes = gdk_texture_downloader_download_bytes (downloader, &texstride);
+  texdata = g_bytes_get_data (texbytes, NULL);
+
+  jpeg_start_compress (&info, TRUE);
+
+  while (info.next_scanline < info.image_height)
+    {
+      row = (guchar *) texdata + info.next_scanline * texstride;
+      jpeg_write_scanlines (&info, &row, 1);
+    }
+
+  jpeg_finish_compress (&info);
+
+  g_bytes_unref (texbytes);
+  g_free (input);
+  jpeg_destroy_compress (&info);
+
+  return g_bytes_new_with_free_func (data, size, (GDestroyNotify) free, NULL);
+}
+
+/*
+ * GObject overrides
+ */
 
 static void
 bs_renderer_finalize (GObject *object)
@@ -190,41 +319,36 @@ bs_renderer_convert_texture (BsRenderer  *self,
                              GdkTexture  *texture,
                              GError     **error)
 {
-  g_autoptr (GdkPixbuf) pixbuf = NULL;
-  g_autofree char *buffer = NULL;
-  size_t buffer_len;
-
   g_return_val_if_fail (BS_IS_RENDERER (self), FALSE);
   g_return_val_if_fail (GDK_IS_TEXTURE (texture), FALSE);
-
-G_GNUC_BEGIN_IGNORE_DEPRECATIONS
-  pixbuf = gdk_pixbuf_get_from_texture (texture);
-G_GNUC_END_IGNORE_DEPRECATIONS
 
   switch (self->image_info.format)
     {
     case BS_IMAGE_FORMAT_BMP:
-      if (!gdk_pixbuf_save_to_buffer (pixbuf,
-                                      &buffer,
-                                      &buffer_len,
-                                      "bmp",
-                                      error,
-                                      NULL))
-        return NULL;
+      {
+        g_autoptr (GdkPixbuf) pixbuf = NULL;
+        g_autofree char *buffer = NULL;
+        size_t buffer_len;
+
+        G_GNUC_BEGIN_IGNORE_DEPRECATIONS
+        pixbuf = gdk_pixbuf_get_from_texture (texture);
+        G_GNUC_END_IGNORE_DEPRECATIONS
+
+        if (!gdk_pixbuf_save_to_buffer (pixbuf,
+                                        &buffer,
+                                        &buffer_len,
+                                        "bmp",
+                                        error,
+                                        NULL))
+          return NULL;
+
+        return g_bytes_new_take (g_steal_pointer (&buffer), buffer_len);
+      }
 
     case BS_IMAGE_FORMAT_JPEG:
-      if (!gdk_pixbuf_save_to_buffer (pixbuf,
-                                      &buffer,
-                                      &buffer_len,
-                                      "jpeg",
-                                      error,
-                                      "quality", "96",
-                                      NULL))
-        return NULL;
+      return convert_to_jpeg (texture);
 
     default:
       g_assert_not_reached ();
     }
-
-  return g_bytes_new_take (g_steal_pointer (&buffer), buffer_len);
 }
