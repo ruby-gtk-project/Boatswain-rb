f0ba4c7e94c06528fc83c4e56c1ec38c163f0c4a
tytan652 <tytan652@tytanium.xyz>
2026-06-21 15:27:44 +0200

    loupedeck: Implement interface claiming
    Part-of: <https://gitlab.gnome.org/World/boatswain/-/merge_requests/45>

--- lib/devices/loupedeck/loupedeck-device.c
+++ lib/devices/loupedeck/loupedeck-device.c
@@ -34,6 +34,17 @@ typedef struct
 {
   int usb_device_fd;
   libusb_device_handle *usb_device_handle;
+
+  struct {
+    uint8_t iface;
+    uint8_t ep_in;
+  } cdc_acm;
+
+  struct {
+    uint8_t iface;
+    uint8_t ep_out;
+    uint8_t ep_in;
+  } cdc_data;
 } LoupedeckDevicePrivate;
 
 static void g_initable_iface_init (GInitableIface *iface);
@@ -65,6 +76,11 @@ loupedeck_device_initable_init (GInitable     *initable,
 {
   LoupedeckDevice *self = LOUPEDECK_DEVICE (initable);
   LoupedeckDevicePrivate *priv = loupedeck_device_get_instance_private (self);
+  libusb_device *device = NULL;
+  struct libusb_config_descriptor *descriptor;
+  const struct libusb_interface_descriptor *cdc_acm_iface;
+  const struct libusb_interface_descriptor *cdc_data_iface;
+  int ret;
 
   BS_ENTRY;
 
@@ -74,6 +90,95 @@ loupedeck_device_initable_init (GInitable     *initable,
                            "File descriptor or handle missing");
       BS_RETURN (FALSE);
     }
+
+  device = libusb_get_device (priv->usb_device_handle);
+  if (!device)
+    {
+      g_set_error_literal (error, G_IO_ERROR, G_IO_ERROR_FAILED,
+                           "Failed to get the underlying USB device");
+      BS_RETURN (FALSE);
+    }
+
+  ret = libusb_get_active_config_descriptor (device, &descriptor);
+  if (ret)
+    {
+      g_set_error (error, G_IO_ERROR, G_IO_ERROR_FAILED,
+                   "Failed to get device config descriptor: %s", libusb_strerror (ret));
+      BS_RETURN (FALSE);
+    }
+
+  if (descriptor->bNumInterfaces < 2)
+    {
+      libusb_free_config_descriptor (descriptor);
+      g_set_error_literal (error, G_IO_ERROR, G_IO_ERROR_FAILED,
+                           "Device has less than 2 interfaces");
+      BS_RETURN (FALSE);
+    }
+
+  for (size_t i = 0; i < descriptor->bNumInterfaces; i++)
+    {
+      const struct libusb_interface_descriptor *iface =
+        &descriptor->interface[i].altsetting[0];
+
+      if (iface->bInterfaceClass == LIBUSB_CLASS_COMM &&
+          iface->bInterfaceSubClass == 0x02 && // Abstract Control Model
+          iface->bInterfaceProtocol == 0x01) // AT Commands: V.250 etc
+        cdc_acm_iface = iface;
+
+      if (iface->bInterfaceClass == LIBUSB_CLASS_DATA)
+        cdc_data_iface = iface;
+    }
+
+  if (!(cdc_acm_iface && cdc_data_iface))
+    {
+      libusb_free_config_descriptor (descriptor);
+      g_set_error_literal (error, G_IO_ERROR, G_IO_ERROR_FAILED,
+                           "Failed to find CDC interfaces");
+      BS_RETURN (FALSE);
+    }
+
+  g_assert (cdc_acm_iface->bNumEndpoints == 1);
+  priv->cdc_acm.ep_in = cdc_acm_iface->endpoint[0].bEndpointAddress;
+  g_assert ((priv->cdc_acm.ep_in & LIBUSB_ENDPOINT_IN) != FALSE);
+
+  g_assert (cdc_data_iface->bNumEndpoints == 2);
+  if (cdc_data_iface->endpoint[0].bEndpointAddress & LIBUSB_ENDPOINT_IN)
+    {
+      priv->cdc_data.ep_out = cdc_data_iface->endpoint[1].bEndpointAddress;
+      priv->cdc_data.ep_in = cdc_data_iface->endpoint[0].bEndpointAddress;
+    }
+  else
+    {
+      priv->cdc_data.ep_out = cdc_data_iface->endpoint[0].bEndpointAddress;
+      priv->cdc_data.ep_in = cdc_data_iface->endpoint[1].bEndpointAddress;
+    }
+
+  g_assert ((priv->cdc_data.ep_out & LIBUSB_ENDPOINT_IN) == FALSE);
+
+  libusb_free_config_descriptor (descriptor);
+
+   g_debug ("Claiming interfaces");
+
+  // NOTE: cdc_acm kernel driver is usually attached
+  libusb_set_auto_detach_kernel_driver (priv->usb_device_handle, TRUE);
+
+  ret = libusb_claim_interface (priv->usb_device_handle, priv->cdc_acm.iface);
+  if (ret)
+    {
+      g_set_error (error, G_IO_ERROR, G_IO_ERROR_FAILED,
+                   "Failed to claim CDC ACM interface: %s", libusb_strerror (ret));
+      BS_RETURN (FALSE);
+    }
+
+  ret = libusb_claim_interface (priv->usb_device_handle, priv->cdc_data.iface);
+  if (ret)
+    {
+      g_set_error (error, G_IO_ERROR, G_IO_ERROR_FAILED,
+                   "Failed to claim CDC Data interface: %s", libusb_strerror (ret));
+      BS_RETURN (FALSE);
+    }
+
+  g_debug ("Interfaces claimed");
 
   // TODO: Implement abstract class
   g_set_error_literal (error, G_IO_ERROR, G_IO_ERROR_NOT_SUPPORTED,
@@ -125,6 +230,9 @@ loupedeck_device_finalize (GObject *object)
     loupedeck_device_get_instance_private (LOUPEDECK_DEVICE (object));
 
   BS_ENTRY;
+
+  libusb_release_interface (priv->usb_device_handle, priv->cdc_data.iface);
+  libusb_release_interface (priv->usb_device_handle, priv->cdc_acm.iface);
 
   g_clear_pointer (&priv->usb_device_handle, libusb_close);
   g_clear_fd (&priv->usb_device_fd, NULL);
